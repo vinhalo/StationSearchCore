e71b22dada93bafb5bfbad39f1eea36154035268
Alvin <alvin.ho@xero.com>
2021-05-24 16:40:46 +0100

    Add test

---
+++ StationSearchCore.Service.Tests/LookupServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace StationSearchCore.Service.Tests
+{
+    [TestFixture]
+    public class LookupServiceTests
+    {
+        [Test]
+        public void SearchForXShouldReturnExpectedResults()
+        {
+            // arrange
+            var s = new LookupService();
+            var expected = new List<string>
+            {
+                "ZZZ"
+            };
+
+            // act
+            var r = s.GetAllStartingWith("Z");
+
+            // assert
+            Assert.That(r, Is.EqualTo(expected));
+        }
+    }
+}
