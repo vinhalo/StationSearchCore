d3c9edbf5b4fd05795fce7220040f02f6b70bead
typesafedev <typesafedev@gmail.com>
2021-06-06 13:58:46 +0100

    Rewrite frontend logic to store application state using mobx

--- StationSearchCore.Web/wwwroot/js/site.js
+++ StationSearchCore.Web/wwwroot/js/site.js
@@ -1,35 +1,48 @@
-﻿function filterStations(e) {
-    const url = "/Stations/GetStations?filter=";
+﻿import { makeAutoObservable, autorun } from 'https://cdn.skypack.dev/mobx';
 
-    fetch(url + e + '"')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            Array.from(document.getElementsByClassName('btn')).forEach(function (btn) {
-                enableStationButtons(btn, data.nextPossibleCharacters);
-                enableBackspaceButton(data.stations);
-                clearSearcResultItems();
+function makeSearchStore() {
+    // Application state
+    const store = makeAutoObservable({
+        stations: [],               // observable
 
-                data.stations.forEach(function (s) {
-                    addSearchResultItem(s);
+        nextPossibleCharacters: [], // observable
+
+        get hasStations() {         // computed. Changes in reaction to stations
+            return this.stations.length > 0
+        },
+
+        get charactersEnabled() {   // computed. Changes in reaction to nextPossibleCharacters
+            const alphabet = [...'ABCDEFGHIJKLMNOPRQRSTUVWXYZ '];
+            return alphabet.map(x => [x, this.nextPossibleCharacters.includes(x)]);
+        },
+
+        filterStations(e) {         // action.
+            const url = "/Stations/GetStations?filter=";
+
+            fetch(`${url}${encodeURIComponent(e)}"`) // Why add d-quote here and remove in backend?
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    store.stations.replace(data.stations);
+                    store.nextPossibleCharacters.replace(data.nextPossibleCharacters);
+                }).catch((error) => {
+                    console.log(error); // network error here
                 });
-            });
-        });
+        }
+    });
+
+    return store;
 }
 
-const enableStationButtons = (btn, nextPossibleCharacters) => {
-    const found = nextPossibleCharacters.includes(btn.innerHTML);
-    btn.disabled = found == false;
-};
+const searchStore = makeSearchStore(); // create singleton store
 
-const enableBackspaceButton = (stations) => {
-    const hasStations = stations.length > 0;
+const enableBackspaceButton = (hasStations) => {
     const backspaceButton = document.querySelector('.backspaceBtn');
     backspaceButton.disabled = hasStations == false;
 };
 
-const clearSearcResultItems = () =>
+const clearSearchResultItems = () =>
     document.querySelector('#searchResults > tbody').innerHTML = '';
 
 const addSearchResultItem = (s) => {
@@ -40,25 +53,43 @@ const addSearchResultItem = (s) => {
     document.querySelector('#searchResults > tbody').appendChild(row);
 };
 
+const enableCharacters = ([alpha, enabled]) => {
+    const a = alpha == " " ? "space" : alpha;
+    const btn = document.querySelector(`[data-alpha="${a}"]`)
+    btn.disabled = enabled == false;
+}
+
+// Hook up reaction to changes in searchStore observables
+autorun(() => {
+
+    enableBackspaceButton(searchStore.hasStations);
+
+    searchStore.charactersEnabled.forEach(enableCharacters);
+
+    clearSearchResultItems();
+    searchStore.stations.forEach(addSearchResultItem);
+});
+
 function changeStationNameFilterText(newfilter) {
     const stationName = document.getElementById('stationName');
     stationName.value = newfilter;
     stationName.dispatchEvent(new Event('change'));
 }
 
+// Hookup event listeners
 (function () {
     const stationName = document.getElementById('stationName');
     stationName.addEventListener('change', function (e) {
-        filterStations(this.value);
+        searchStore.filterStations(this.value);
     });
     stationName.addEventListener('paste', function (e) {
-        filterStations(this.value);
+        searchStore.filterStations(this.value);
     });
     stationName.addEventListener('keyup', function (e) {
-        filterStations(this.value);
+        searchStore.filterStations(this.value);
     });
 
-    Array.from(document.getElementsByClassName('btn')).forEach(function (el) {
+    document.querySelectorAll('.btn').forEach(function (el) {
         el.addEventListener('click', function () {
             const currentText = stationName.value;
             changeStationNameFilterText(currentText + this.textContent);
